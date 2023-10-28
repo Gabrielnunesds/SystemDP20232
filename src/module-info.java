ec0db367b432fb85ca9e648bcd0b6c910f19ff8a
gabrielnds <bielnunao2013@gmail.com>
2023-10-27 21:29:05 -0300

    MVP + Composição System

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module SystemDP20232 {
+}
---
+++ src/tech/gabrielnunesds/app/AppMain.java
@@ -0,0 +1,15 @@
+package tech.gabrielnunesds.app;
+
+import tech.gabrielnunesds.pessoas.Cliente;
+import tech.gabrielnunesds.pessoas.Pessoa;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Cliente cli1 = new Cliente();
+		cli1.cadastrar();
+	}
+
+}
